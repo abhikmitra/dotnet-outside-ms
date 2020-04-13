8dd518987f2841980822e06b7fcf73b3a5d688a8
Abhik Mitra <abmitra@microsoft.com>
2020-04-13 19:26:46 +0530

    Is Containerized API

---
+++ BackendApp/BackendAPI/Controllers/EnvironmentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnvironmentController : ControllerBase
+    {
+        private const string CONTAINERIZED = "containerized";
+        private readonly ILogger<EnvironmentController> _logger;
+
+        public EnvironmentController(ILogger<EnvironmentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("iscontainerized")]
+        public bool IsRunningInContainer()
+        {
+            var isContainerized = Environment.GetEnvironmentVariable(CONTAINERIZED);
+            if (isContainerized == null)
+            {
+                return false;
+            }
+
+            return bool.Parse(isContainerized);
+        }
+    }
+}
